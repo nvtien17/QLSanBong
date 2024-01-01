dbf7972a60cc391d58cbfb54e64922aa72e96dd7
Thang280602 <thang280602@gmail.com>
2024-01-01 21:42:51 +0700

    UpdateLogin

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,18 +1,27 @@
 import { Link } from "react-router-dom";
 import './login.css';
+import { useNavigate } from 'react-router-dom';
 
-function login() {
+
+
+function Login() {
+    const navigate=useNavigate();
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        
+        navigate('/');
+    };
     return (
-        <form className="login-container" method="post">
+        <form className="login-container" method="post" onSubmit={handleSubmit}>
             <h1 className="login-title">Đăng nhập</h1>
             <input className="login-textbox" placeholder="Tên Đăng nhập" name="username" required/>
-            <input className="login-textbox" placeholder="Mật khẩu" name="password" required/>
+            <input className="login-textbox" type="password" placeholder="Mật khẩu" name="password" required/>
             <div className="login-container-button">
-                <button className="login-button" type="submit">Đăng nhập</button>
+                <button className="login-button" type="submit" >Đăng nhập</button>
                 <Link to='/register' className="login-button dangky">Đăng ký</Link>
             </div>
         </form>
     )
 }
 
-export default login
+export default Login
--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -1,13 +1,20 @@
 import { Link } from "react-router-dom";
 import './register.css';
-function register() {
+import { useNavigate } from 'react-router-dom';
+function Register() {
+    const navigate=useNavigate();
+    const handleSubmitLogin = (event) => {
+        event.preventDefault();
+        
+        navigate('/login');
+    };
     return (
-        <form className="register-container" method="post">
+        <form className="register-container" method="post" onSubmit={handleSubmitLogin}>
             <h1 className="register-title">Đăng ký</h1>
             <input className="register-textbox" placeholder="Họ và tên" name="name" required/>
             <input className="register-textbox" placeholder="Số điện thoại" name="phone" required/>
             <input className="register-textbox" placeholder="Tên đăng nhập" name="username" required/>
-            <input className="register-textbox" placeholder="Mật khẩu" name="password" required/>
+            <input className="register-textbox" placeholder="Mật khẩu" name="password" type="password" required/>
             <button className="register-button" type="submit">Đăng ký</button>
             <div className="link-login">
                 Bạn đã có tài khoản?
@@ -17,4 +24,4 @@ function register() {
     )
 }
 
-export default register
+export default Register
